af86c775d6ed311363c224d9a50e5b1a509370c3
TJ James <jamestw13@gmail.com>
2021-09-30 18:21:32 -0500

    included query parameter to make single-repo.html pull issues list from repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,14 @@
 var issueContainerEl = document.querySelector("#issue-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+	var queryString = document.location.search;
+	var repoName = queryString.split("=")[1];
+	console.log(repoName);
+	getRepoIssues(repoName);
+	repoNameEl.textContent = repoName;
+};
 
 var getRepoIssues = function (repo) {
 	// create a url endpoint
@@ -70,4 +79,4 @@ var displayWarning = function (repo) {
 	limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
