cd42c5759f140121e40f806b05a59a49b7f9a0b7
TJ James <jamestw13@gmail.com>
2021-09-30 19:41:25 -0500

    added featured search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
 	event.preventDefault();
@@ -17,6 +18,31 @@ var formSubmitHandler = function (event) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function (event) {
+	var language = event.target.getAttribute("data-language");
+
+	if (language) {
+		getFeaturedRepos(language);
+		// clear old content
+		repoContainerEl.textContent = "";
+	}
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
+var getFeaturedRepos = function (language) {
+	var apiUrl = `https://api.github.com/search/repositories?q=${language}+is:featured&sort=help-wanted-issues`;
+	fetch(apiUrl).then(function (response) {
+		if (response.ok) {
+			response.json().then(function (data) {
+				displayRepos(data.items, language);
+			});
+		} else {
+			alert("Error: Github User Not Found");
+		}
+	});
+};
 
 var displayRepos = function (repos, searchTerm) {
 	// check if api returned any repos
